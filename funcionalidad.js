a16ec0fe07f1e8ab7fe42f13db8c9e9553f32b0a
tomasdaureli <tomasdaureli@gmail.com>
2022-06-09 21:44:26 -0300

    JavaScript

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,13 +1,22 @@
-
-function cargarFormulario() {
-    let boton = document.getElementById("boton");
-    boton.onclick = validarFormulario;
-}
+let boton = document.getElementById("boton");
+boton.onclick = validarFormulario;
 
 function validarFormulario() {
-    let fecha = document.getElementById("fecha");
-    if (fecha.value == null) {
+    const fecha = document.getElementById("fecha");
+    const horario = document.getElementById("horario");
+    const personas = document.getElementById("personas");
+    if (fecha.value == "") {
         alert("Debe ingresar una fecha.");
+        return false;
     }
-    alert("Hola mundo");
+    if (horario.value == ""){
+        alert("Debe ingresar el horario");
+        return false;
+    }
+    if (personas.value == "") {
+        alert("Debe ingresar la cantidad de personas");
+        return false;
+    }
+    confirm("Hola mundo!");
+    return true;
 }
