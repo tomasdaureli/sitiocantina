bdb9884da5132941742464a32d3418433034d064
tomasdaureli <tomasdaureli@gmail.com>
2022-06-23 15:55:38 -0300

    cambio js

--- funcionalidad.js
+++ funcionalidad.js
@@ -13,9 +13,9 @@ const form_modal = document.getElementById("formulario_modal");
 var fecha = new Date();
 var anio = fecha.getFullYear();
 var dia = fecha.getDate();
-var _mes = fecha.getMonth();//viene con valores de 0 al 11
-_mes = _mes + 1;//ahora lo tienes de 1 al 12
-if (_mes < 10)//ahora le agregas un 0 para el formato date
+var _mes = fecha.getMonth();
+_mes = _mes + 1;
+if (_mes < 10)
 { var mes = "0" + _mes;}
 else
 { var mes = _mes.toString;}
